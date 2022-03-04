e956e906eccb45ddf63fcb1861c4b26f8ef939fc
blarth <jminocente@gmail.com>
2022-03-04 00:20:12 -0300

    feat: initial configs, dependencies installed, db with deploy

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import cors from "cors";
+import express, { json } from "express";
+import router from "./routes/index.js";
+import 'dotenv/config';
+
+
+const server = express();
+
+
+server.use(cors());
+server.use(json());
+server.use(router)
+
+
+export default router;
+
+server.listen(process.env.PORT, () => {
+    console.log("Server is listening on port " + process.env.PORT);
+});
+
